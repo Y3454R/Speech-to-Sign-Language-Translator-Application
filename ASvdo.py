dcd8ad61104d574d49695fdc752a2e1063f1ed74
Ashikur Rahman <58724901+Ashik-90@users.noreply.github.com>
2021-06-22 21:01:44 +0600

    Update ASvdo.py

--- ASvdo.py
+++ ASvdo.py
@@ -1,9 +1,9 @@
-import cv2,os,glob
+import cv2,os,glob, shutil
 from os.path import isfile, join
 from PIL import Image
-import nltk
+import nltk, datetime
+import speech_recognition as sr
 
-#Can be modified when user changes setting of delay for each frame
 fps = 1
 time = 2
 
@@ -14,42 +14,53 @@ def img_to_vid(pthIn, pthOut):
     for i in range (len(files)):
         filename = pthIn+files[i]
         img = cv2.imread(filename)
-        height, width, layers = img.shape
-        size = (width, height)
 
         for k in range(time):
             frame_array.append(img)
-
-    out = cv2.VideoWriter(pthOut, cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
+                                                          # 125x205 is dimension of resized img
+    out = cv2.VideoWriter(pthOut, cv2.VideoWriter_fourcc(*'mp4v'), fps, (281,363) )
     for i in range(len(frame_array)):
         out.write(frame_array[i])
     out.release()
 
+
 def fetch_img(text):
     new_name_index = 0
-    for letter in text:
-        if letter == ' ':
-            continue
-        else:
-            try:
-                im = Image.open('C:/Users/user/PycharmProjects/videomake/ResizedImage/'+ letter + '.png')
-                im.save('C:/Users/user/PycharmProjects/videomake/Final/' + str(new_name_index)+ '.png')
+    for word in text:
+        try:
+            im = Image.open('C:/Users/user/PycharmProjects/videomake/ResizedImage/'+ word + '.png')
+            im.save('C:/Users/user/PycharmProjects/videomake/Final/' + str(new_name_index)+ '.png')
+            new_name_index += 1
+        except:
+            for letter in word.lower():
+                try:
+                    im = Image.open('C:/Users/user/PycharmProjects/videomake/ResizedImage/'+ letter + '.png')
+                    im.save('C:/Users/user/PycharmProjects/videomake/Final/' + str(new_name_index)+ '.png')
+                    new_name_index += 1 #important  (same new_name_index replaces image)
+                except:
+                    im = Image.open('C:/Users/user/PycharmProjects/videomake/ResizedImage/idk.png')
+                    im.save('C:/Users/user/PycharmProjects/videomake/final/'+ str(new_name_index)+'.png')
+                    new_name_index += 1  #important (same new_name_index replaces image)
+    
+        
 
-            except:
-                im = Image.open('C:/Users/user/PycharmProjects/videomake/ResizedImage/idk.png')
-                im.save('C:/Users/user/PycharmProjects/videomake/final/'+
-                                str(new_name_index)+'.png')
-        new_name_index += 1
-
-
-def del_punc(sentence):
+def del_punc(tokens):
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    no_punct = ""
-    for char in sentence:
-        if char not in punctuations:
-            no_punct = no_punct + char
+    for member in tokens:
+        if member in punctuations:
+            tokens.remove(member)
     # display the unpunctuated string
-    return no_punct
+    print('tok', tokens)
+    return tokens
+
+def tokenize(voice_text):
+    tokens = nltk.word_tokenize(voice_text)
+    not_necessary = ['is', 'the', 'in', 'of', 'at', 'after', 'am', 'before', 'for', 'a', 'an']
+    for element in tokens:
+        if element in not_necessary:
+            tokens.remove(element)
+    print(tokens)
+    return tokens
 
 
 def clear_Final_folder(dir):
@@ -58,22 +69,44 @@ def clear_Final_folder(dir):
     for filename in files:
         os.unlink(filename)
 
-
-import speech_recognition as sr
-from kivy.properties import StringProperty
-
-r = sr.Recognizer()
-#dure = int(input("Set duration"))
-dure = 4
-
-
-def record():
-    with sr.Microphone() as source:
-        print("Speak now")
-        audio = r.record(source, duration=dure)
+def audio_to_text(dir):
+    r = sr.Recognizer()
+    with sr.AudioFile(dir) as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
         try:
-            output = r.recognize_google(audio)
-
+            text = r.recognize_google(audio)
         except:
-            output = "Could not recognize"
-    return output
+            text = 'null'
+    return text
+
+
+def text_to_text(dir):
+    text_file = open(dir, 'r')
+    text = ''
+    lines = text_file.readlines()
+    for i in lines:
+        text += i + ' '
+    return text
+
+def Backup_history(source, dest):
+    current_time = datetime.datetime.now()
+    shutil.copy(source, dest)
+    #unique filename generator fnm
+    fnm = str(current_time.hour) + '_' + str(current_time.minute) + '_'+ str(current_time.second) + '_' + str(current_time.year) + '_' + str(current_time.month)
+    os.rename(dest+'/vid_ex.mp4', dest+fnm+'.mp4')
+
+import urllib.request
+def connect(host='http://google.com'):
+    try:
+        urllib.request.urlopen(host) #Python 3.x
+        return True
+    except:
+        return False
+        
+def Update_Database(url):
+    dest = 'C:/Users/user/PycharmProjects/videomake/ResizedImage'
+    shutil.copy(url, dest)
+
+    #unique filename generator fnm
+    #os.rename(dest+'/vid_ex.mp4', dest+fnm+'.mp4')
